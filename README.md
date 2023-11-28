2ba1d2b523f921c8e2e145f1912931198e41cf5d
Chris Bentley <chris.r.bentley@gmail.com>
2023-11-27 21:09:47 -0500

    implement all basic features

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Calculator
 
 Calculator project created with HTML, CSS, and JS
+
+[Live Demo](https://cbentley.github.io/calculator/)
--- script.js
+++ script.js
@@ -1,40 +1,119 @@
-let num1;
-let num2;
-let operator;
+const calculator = document.getElementById('calculator');
+const display = calculator.querySelector('.display');
+const operandButtons = calculator.querySelectorAll('.operand');
+const operatorButtons = calculator.querySelectorAll('.operator');
+const equalsButton = calculator.querySelector('.equals');
+const clearButton = calculator.querySelector('.clear');
+const divideByZeroMessage = 'Nope :)';
 
-function add(num1, num2) {
-    return num1 + num2;
+let operand1 = '';
+let operand2 = '';
+let operator = null;
+
+operandButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (operand1 === divideByZeroMessage) {
+            operand1 = button.textContent;
+            display.textContent = operand1;
+            return;
+        }
+        
+        if (display.textContent.length > 11) {
+            return;
+        }
+
+        if (operator === null) {
+            operand1 += button.textContent;
+            display.textContent = operand1;
+        } else {
+            operand2 += button.textContent;
+            display.textContent = operand2;
+        }
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (operand1 !== divideByZeroMessage && operand1 !== '' && operand2 === '') {
+            operator = button.value;
+        } else if (operand1 !== divideByZeroMessage && operand1 !== '' && operand2 !== '') {
+            [operand1, operand2, operator] = operate(operand1, operand2, operator);
+            operator = button.value;
+        }
+    });
+});
+
+equalsButton.addEventListener('click', () => {
+    if (operand1 !== divideByZeroMessage && operand1 !== '' && operand2 !== '') {
+        [operand1, operand2, operator] = operate(operand1, operand2, operator);
+    }
+});
+
+clearButton.addEventListener('click', () => {
+    display.textContent = '0';
+    operand1 = '';
+    operand2 = '';
+    operator = null;
+});
+
+function add(operand1, operand2) {
+    return operand1 + operand2;
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+function subtract(operand1, operand2) {
+    return operand1 - operand2;
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
+function multiply(operand1, operand2) {
+    return operand1 * operand2;
 }
 
-function divide(num1, num2) {
-    return num1 / num2;
+function divide(operand1, operand2) {
+    if (operand2 === 0) {
+        return divideByZeroMessage;
+    }
+    return operand1 / operand2;
 }
 
-function operate(num1, num2, operator) {
+function isFloat(number) {
+    return !Number.isInteger(number) && Number.isFinite(number);
+}
+
+function roundToDecimalPlaces(number, decimalPlaces) {
+    const roundedString = number.toFixed(decimalPlaces);
+    const roundedNumber = parseFloat(roundedString); // or Number(roundedString)
+    return roundedNumber;
+}
+
+function operate(operand1, operand2, operator) {
+    console.log(operand1, operand2, operator);
     let result;
+    operand1 = Number(operand1);
+    operand2 = Number(operand2);
 
     if (operator === '+') {
-        result = add(num1, num2);
+        result = add(operand1, operand2);
     } else if (operator === '-') {
-        result = subtract(num1, num2);
+        result = subtract(operand1, operand2);
     } else if (operator === '*') {
-        result = multiply(num1, num2);
+        result = multiply(operand1, operand2);
     } else if (operator === '/') {
-        result = divide(num1, num2);
+        result = divide(operand1, operand2);
     }
 
-    return result;
+    if (isFloat(result)) {
+        result = roundToDecimalPlaces(result, 3);
+    }
+
+    display.textContent = result.toString();
+    operand1 = result.toString();
+    operand2 = '';
+    operator = null;
+
+    return [operand1, operand2, operator]
 }
 
-console.log(operate(3, 2, '+'));
-console.log(operate(3, 2, '-'));
-console.log(operate(3, 2, '*'));
-console.log(operate(3, 2, '/'));
+// console.log(operate(3, 2, '+'));
+// console.log(operate(3, 2, '−'));
+// console.log(operate(3, 2, '×'));
+// console.log(operate(3, 2, '÷'));
